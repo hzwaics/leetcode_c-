72f286325b1de5fadaae62c2ac83b4948691807a
hzwaics <hzwaics@163.com>
2020-01-14 13:07:30 +0800

    fix bug

--- Algorithm/design/LRUCache.cpp
+++ Algorithm/design/LRUCache.cpp
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <iostream>
 #include <map>
+#include <unistd.h> 
 
 using namespace std;
 
@@ -24,11 +25,18 @@ private:
     int size;
 
 public:
+    // 默认各初试化头尾node节点
     DoubleLinkedList() {
-        pHead = pTail = NULL;
-        size = 0;
+        pHead = new Node(0,0);
+        pTail = new Node(0,1);
+        pHead->next = pTail;
+        pHead->prev = NULL;
+        pTail->next = NULL; 
+        pTail->prev = pHead;
+        size = 0;  
     }
 
+    // 删除所有的node节点
     ~DoubleLinkedList() {
         while(pHead != NULL) {
             Node *p = pHead;
@@ -46,30 +54,42 @@ public:
     // 总是在表头插入节点数据
     void addNodeHead(Node *n) {
         size++;
-        if (pHead == NULL) {
-            pHead = pTail = n;
-            // return n;
-        }
-        n->next = pHead;
-        n->prev = NULL;
-        pHead->prev = n;
-        pTail = pHead;//更新头尾节点为n
-        pHead = n;
+
+        n->next = pHead->next;
+        n->prev = pHead;
+        pHead->next->prev = n;
+        pHead->next = n;
+
+        return;
 
         // return n;//返回头部节点指针
     }
 
-    void moveToHead(Node *n) {
+    // 独立一个node节点出来
+    void unlinkNode(Node *n) {
         n->prev->next = n->next;
         n->next->prev = n->prev;
 
-        n->next = pHead;
         n->prev = NULL;
-        pHead = n;
+        n->next = NULL;
+    }
 
+    void moveToHead(Node *n) {
+        if (n == pHead->next)
+            return;
+        else {
+            // 先独立出来这个node节点
+            unlinkNode(n);
+            // 再移动到表头
+            addNodeHead(n);
+
+        }
+
+        return;
         // return n;
     }
 
+    // 支持删除data节点
     void deleteNode(Node *n) {
         n->prev->next = n->next;
         n->next->prev = n->prev;
@@ -80,17 +100,16 @@ public:
     }
 
     void popTailNode() {
-        pTail->prev->next = NULL;
-        Node* tmp = pTail;
-        pTail = pTail->prev;
-
-        size--;
+        Node* tmp = pTail->prev;
+        tmp->prev->next = pTail;
+        pTail->prev = tmp->prev;
 
         delete tmp;
+        
     }
 
     Node* getTailNode() {
-        return pTail;
+        return pTail->prev;
     }
 
 };
@@ -109,14 +128,15 @@ private:
 
 public:
     LRUCache(int capacity) {
-        count = 0;
-        capacity = capacity;
+        this->count = 0;
+        this->capacity = capacity;
         
     }
     
     int get(int key) {
         // Node* n = cache.get(key);
         if (cache.find(key) == cache.end()) {
+            // cout << "error: -1" << endl;
             return -1;      // cache里面没有
         }
         Node* n = cache[key];
@@ -125,7 +145,7 @@ public:
         //cache 命中，移动到队列头部
         L.moveToHead(n);
 
-        cout << "get" << n->value << endl;
+        // cout << "get: " << n->value << endl;
 
         return n->value;
     }
@@ -144,19 +164,24 @@ public:
 
             ++count;
 
-            if (count > capacity) {
-                // 最后一个节点弹出
-                L.popTailNode();
+            if (count > this->capacity) {
                 // 删除tail节点的缓存
                 int tmp = L.getTailNode()->key;
                 cache.erase(tmp);
-
+                // 最后一个节点弹出
+                L.popTailNode();
 
                 count--;
             }
         } else {
-            Node* n = cache[key];
-            L.moveToHead(n);
+            // delete node节点 & map缓存
+            L.deleteNode(cache[key]);
+            cache.erase(key);
+
+            // 重新新建node，写入map缓存并插入队列头部
+            Node* n = new Node(key, value);
+            cache[key] = n;
+            L.addNodeHead(n);
         }
         
     }
@@ -170,9 +195,18 @@ int main(int argc, char** argv) {
 
     LRUCache cache(capacity);
 
-    cache.put(1,1);
-    cache.put(2,2);
-    cache.get(1);
+    for (int i=0; i<10000; i++) {
+        cache.put(2,1);
+        cache.put(2,2);
+        cache.get(2);
+        cache.put(1,1);
+        cache.put(4,1);
+        cache.get(2);
+
+        sleep(3);
+
+    }
+
 
     return 0;
 }
